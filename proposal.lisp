f32e6d1d68daa562064edfe6254f23cf2d25d835
Charlie Greenbacker <greenbacker@gmail.com>
2011-02-14 16:58:39 -0500

    progress update

--- proposal.lisp
+++ proposal.lisp
@@ -1,3 +1,10 @@
+#|
+:ld ~/Sparser/load-nlp.lisp
+:ld ~/Sparser/proposal.lisp
+(in-package :sparser)
+(cfg-setting)
+|#
+
 (in-package :sparser)
 (cfg-setting)
 
@@ -9,28 +16,60 @@
 (defun der (edge)
   (d (edge-referent (e# edge))))
 
-(defun med-all ()
+(defun med-full ()
   (p "Will Medtronic's pulse quicken?
 
-Medical device giant Medtronic (MDT), the leader in
-defibrillators and pacemakers, has been a market laggard. The stock
-slumped from 60 in January to 49.19 on May 17. But some investors say
-it may beat analysts' consensus forecast of 62 cents a share when it
-reports earnings for its fiscal fourth quarter on May 23. Among the
-few bulls: Investment firm Harris Nesbitt's Joanne Wuensch, who
-continues to rate it \"outperform,\" with a 12-month target of 62.
-Still, some worry Medtronic may deliver bad news. Not only has growth
-slowed in the cardiology market, but rival St. Jude Medical (SJM) also
-missed its quarterly sales forecast. Wuensch counters that Medtronic
-is \"more insulated from the implantable-device market sways than its
-brethren.\" It generates 27% of revenues from them, vs. St. Jude's
-36%. Wuensch sees Medtronic earning $2.09 a share on sales of $11.3
-billion this year, and $2.38 on $12.6 billion in 2007. David Sowerby,
-portfolio manager at investment firm Loomis Sayles, which owns shares,
-says the stock is \"compelling\" near its 10-year low, especially as
-he expects Medtronic to gain market share and show double-digit
-earnings growth in 2007.")
-)
+Medical device giant Medtronic (MDT), the leader in defibrillators and pacemakers, has been a market laggard. The stock slumped from 60 in January to 49.19 on May 17. But some investors say it may beat analysts' consensus forecast of 62 cents a share when it reports earnings for its fiscal fourth quarter on May 23. Among the few bulls: Investment firm Harris Nesbitt's Joanne Wuensch, who continues to rate it \"outperform,\" with a 12-month target of 62. Still, some worry Medtronic may deliver bad news. Not only has growth slowed in the cardiology market, but rival St. Jude Medical (SJM) also missed its quarterly sales forecast. Wuensch counters that Medtronic is \"more insulated from the implantable-device market sways than its brethren.\" It generates 27% of revenues from them, vs. St. Jude's 36%. Wuensch sees Medtronic earning $2.09 a share on sales of $11.3 billion this year, and $2.38 on $12.6 billion in 2007. David Sowerby, portfolio manager at investment firm Loomis Sayles, which owns shares, says the stock is \"compelling\" near its 10-year low, especially as he expects Medtronic to gain market share and show double-digit earnings growth in 2007."))
+
+(defun med-0 ()
+  (p "Will Medtronic's pulse quicken?"))
+
+(defun med-1 ()
+  (p "Medical device giant Medtronic (MDT), the leader in defibrillators and pacemakers, has been a market laggard."))
+
+(defun med-2 ()
+  (p "The stock slumped from 60 in January to 49.19 on May 17."))
+
+(defun med-3 ()
+  (p "But some investors say it may beat analysts' consensus forecast of 62 cents a share when it reports earnings for its fiscal fourth quarter on May 23."))
+
+(defun med-4 ()
+  (p "Among the few bulls: Investment firm Harris Nesbitt's Joanne Wuensch, who continues to rate it \"outperform,\" with a 12-month target of 62."))
+
+(defun med-5()
+  (p "Still, some worry Medtronic may deliver bad news."))
+
+(defun med-6 ()
+  (p "Not only has growth slowed in the cardiology market, but rival St. Jude Medical (SJM) also missed its quarterly sales forecast."))
+
+(defun med-7 ()
+  (p "Wuensch counters that Medtronic is \"more insulated from the implantable-device market sways than its brethren.\""))
+
+(defun med-8 ()
+  (p "It generates 27% of revenues from them, vs. St. Jude's 36%."))
+
+(defun med-9 ()
+  (p "Wuensch sees Medtronic earning $2.09 a share on sales of $11.3 billion this year, and $2.38 on $12.6 billion in 2007."))
+
+(defun med-10 ()
+  (p "David Sowerby, portfolio manager at investment firm Loomis Sayles, which owns shares, says the stock is \"compelling\" near its 10-year low, especially as he expects Medtronic to gain market share and show double-digit earnings growth in 2007."))
+
+(defun med-all ()
+  (med-0)
+  (med-1)
+  (med-2)
+  (med-3)
+  (med-4)
+  (med-5)
+  (med-6)
+  (med-7)
+  (med-8)
+  (med-9)
+  (med-10))
+
+#| GRAMMAR |#
+
+(define-company "Medtronic")
 
 ;; stock ticker symbol
 
