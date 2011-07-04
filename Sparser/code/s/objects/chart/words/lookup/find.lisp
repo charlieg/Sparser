a05c2774fba7990306d5d54f08d9179121dae87e
David D. McDonald <davidmcdonald@alum.mit.edu>
2011-07-04 18:20:59 -0400

    Reverted unneeded change

--- Sparser/code/s/objects/chart/words/lookup/find.lisp
+++ Sparser/code/s/objects/chart/words/lookup/find.lisp
@@ -1,16 +1,11 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992  David D. McDonald  -- all rights reserved
-;;; extensions copyright (c) 2007-2008 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
+;;; copyright (c) 1992 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "find"
 ;;;   Module:  "objects;words:lookup"
-;;;  Version:  June 2008
+;;;  Version:  January 1992
 
 ;; initiated 1/16/92
-;; 6/2/08 Added *force-case-shift* so "I" and "i" would be identical
-;;  when that flag is up, also added override for non-word cases
-;;  like section markers.
 
 (in-package :sparser)
 
