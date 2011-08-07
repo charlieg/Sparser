83a8e905eca099d20b48b70a4d947127248fefa3
David D. McDonald <davidmcdonald@alum.mit.edu>
2011-08-07 18:16:23 -0400

    junction and such

---
+++ Sparser/code/s/grammar/model/core/places/configurations.lisp
@@ -0,0 +1,13 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2011  David D. McDonald  -- all rights reserved
+;;; $Id:$
+;;;
+;;;     File:  "configurations"
+;;;   Module:  "model;core:places:"
+;;;  version:  August 2011
+
+;; initated 8/3/11
+
+(in-package :sparser)
+
+;; e.g junction, split, fork, cross
