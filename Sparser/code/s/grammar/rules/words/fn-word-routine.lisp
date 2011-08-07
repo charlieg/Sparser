6b7a8a20f89a6f2dd4cc201c6bce9dcb6d385e2c
David D. McDonald <davidmcdonald@alum.mit.edu>
2011-08-07 17:12:57 -0400

    Factored out the bracket-assigning as its own routine

--- Sparser/code/s/grammar/rules/words/fn-word-routine.lisp
+++ Sparser/code/s/grammar/rules/words/fn-word-routine.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992,1993,1994.1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2011  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "fn word routine"
 ;;;    Module:   "grammar;rules:words:"
-;;;   Version:   0.7 May 1995
+;;;   Version:   0.7 July 2011
 
 ;; 0.1 (12/17/92 v2.3) redid the routine so it was caps insensitive and handled
 ;;      bracketing.
@@ -15,6 +15,8 @@
 ;; 0.6 (10/24) embelleshed check to let brackets come in as bracket objects
 ;; 0.7 (11/16) added keyword for form category
 ;;     (5/12/95) fixed glitch in format stmt.
+;;     (7/31/11) factored out backet operations as assign-brackets-to-word.
+;;      and added make-minimal-word-form-rule
 
 (in-package :sparser)
 
@@ -32,17 +34,11 @@
                              &key ((:brackets bracket-symbols))
                                   ((:form name-of-form-category)))
 
-  (let ((word (resolve-string-to-word/make string))
+  (let ((word (or (when (word-p string) string)
+                  (when (polyword-p string) string)
+                  (resolve-string-to-word/make string)))
         (form (if name-of-form-category
-                (let ((category
-                       (etypecase name-of-form-category
-                         (symbol (category-named name-of-form-category))
-                         ((or referential-category mixin-category category)
-                          name-of-form-category))))
-                  (unless category
-                    (break "The category ~A isn't defined yet"
-                           name-of-form-category))
-                  category )
+                (resolve-form-category name-of-form-category)
                 t )))
 
     (etypecase word
@@ -54,23 +50,35 @@
 
     (unless (rule-set-for word)
       (establish-rule-set-for word))
-    
-    (when bracket-symbols
-      (delete-existing-bracket-assignments word)
-      (let ( bracket )
-        (dolist (bracket-symbol bracket-symbols)
-          (unless (or (bracket-p bracket-symbol)
-                      (and (symbolp bracket-symbol)
-                           (boundp bracket-symbol)))
-            (error "The ostensive bracket symbol ~A isn't defined"
-                   bracket-symbol))
 
-          (setq bracket (eval bracket-symbol))
-          (unless (bracket-p bracket)
-            (error "The symbol ~A evals to ~A~%instead of to a bracket"
-                   bracket-symbol bracket))
-
-          (assign-bracket/expr word bracket))))
+    (assign-brackets-to-word word bracket-symbols)
 
     word ))
 
+
+(defun make-minimal-word-form-rule (word form-label)
+  (let ((form-cat (resolve-form-category form-label)))
+    (define-cfr word (list word)
+      :form form-cat
+      :referent word
+      :source :form)))
+  
+
+;;--- auxiliaries
+
+(defun resolve-form-category (form-label)
+  (let ((category
+         (etypecase form-label
+           (symbol (category-named form-label))
+           ((or referential-category mixin-category category)
+            form-label))))
+    (unless category
+      (break "The category ~A isn't defined yet"
+             name-of-form-category))
+    (unless (form-category? category)
+      (cerror "Just keep going"
+              "You are proposing to use the category ~a~
+             ~%as the form of a rule, but it's not defined as~
+             ~%a form rule. See /grammar/rules/syntax/categories.lisp"
+              form-label))
+    category))
