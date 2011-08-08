54ba18945606db813100ef3e5e2cdb838325c80a
David D. McDonald <davidmcdonald@alum.mit.edu>
2011-08-07 21:10:58 -0400

    Added weak location

--- Sparser/code/s/grammar/rules/tree-families/transitive.lisp
+++ Sparser/code/s/grammar/rules/tree-families/transitive.lisp
@@ -1,11 +1,11 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-1998 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1998,2011 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2007-2009 BBNT Solutions LLC. All Rights Reserved
 ;;; $Id:$
 ;;;
 ;;;     File:  "transitive"
 ;;;   Module:  "grammar;rules:tree-families:"
-;;;  version:  0.3 April 2009
+;;;  version:  0.3 August 2011
 
 ;; initiated 8/5/92 v2.3, added passives 8/24
 ;; 0.1 (10/13) reorganized (in)transitive to fit the paper
@@ -14,7 +14,8 @@
 ;;     (7/23/98) Added transitive/new-head
 ;;     (6/26/07) Added transitive/pp
 ;; 0.3 (1/29/08) In intransitive, changed the vg to vp
-;;   (4/20/09) moved transitive/pp to verbs-taking-pps in order to specialize it
+;;     (4/20/09) moved transitive/pp to verbs-taking-pps in order to specialize it
+;;     (8/7/11) moved in transitive-location, which isn't all that well designed. 
 
 (in-package :sparser)
 
@@ -75,4 +76,25 @@
                        :head right-edge
                        :binds (patient left-edge)))
       ))
+
+
+;; Was in checkpoint/vocabulary, but surely doesn't belong there
+;; Note at the time says it can't be resolved then.
+(define-exploded-tree-family  transitive-location
+  :description ""
+  :binding-parameters ( agent  location )
+  :labels ( s vp vg np/subject loc1 loc2 loc3 )
+  :cases
+     ((:subject (s  (np/subject vp)
+                 :head right-edge
+                 :binds (agent left-edge)))
+      (:deictic-loc (vp (vg loc1) ;; deictic-location -- only in literals
+		      :head left-edge
+		      :binds (location right-edge)))
+      (:location (vp (vg loc2) ;; location)
+		    :head left-edge
+		    :binds (location right-edge)))
+      (:location (vp (vg loc3) ;; spatial-orientation)
+		    :head left-edge
+		    :binds (location right-edge)))))
 
